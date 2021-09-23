8195c7469cd4397a9241134beeebae2f2e230e59
zfj <zfj/PC_computer>
2021-09-23 21:59:44 +0800

    firt runnable line chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -11,17 +11,37 @@
         ></el-tree>
       </el-aside>
       <el-main>
+        <el-row justify="start" align="middle" >
+          <el-col :span="3">
+            请选择要绘制的测点
+          </el-col>
+          <el-col :span="3">
+            <el-select
+              v-model="selected_points"
+              multiple placeholder="选择测点"
+              collapse-tags
+              :loading="points_loading"
+            >
+              <el-option
+                v-for="item in points"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="2">
+            <el-button type="primary" @click="handleDraw">绘制</el-button>
+          </el-col>
+        </el-row>
         <div class="chart-block">
           <ECharts
               class="chart"
               ref="chart"
               :option="option"
-              :setOptionOpts="{ notMerge: true }"
               :loading="loading"
               :loadingOpts="{ text: 'Wait for 0.5s' }"
-              :events="[
-                  ['dblclick', onDblclick],
-              ]"
           />
         </div>
       </el-main>
@@ -35,7 +55,10 @@ import BottomFooter from './Footer.vue'
 import { createComponent } from 'echarts-for-vue'
 import * as echarts from 'echarts'
 import { h } from 'vue'
-import { Option1 } from './config';
+import { baseMap } from './config'
+import axios from 'axios';
+import _ from 'lodash';
+import moment from 'moment';
 
 export default {
   name: 'Chart',
@@ -55,25 +78,130 @@ export default {
         value: 1,
       }],
       picNum: null,
-      option: Option1
+      option: baseMap,
+      points: [],
+      points_loading: true,
+      selected_points: []
     }
   },
-    methods: {
-      handleNodeClick(node) {
-        if (!node.value) {
-          return null
-        }
-        this.picNum = node.value
-      },
-      doSomething() {
-        this.$refs.chart.inst.getWidth();
+  mounted: function() {
+    axios
+      .get('http://localhost:8000/mpoints/?section__name=DC4')
+      .then(response => {
+        response.data.forEach(item => {
+          this.points.push({
+            'value': item.name,
+            'label': item.name
+          })
+        });
+        this.points_loading=false;
+      });
+  },
+  methods: {
+    handleNodeClick(node) {
+      if (!node.value) {
+        return null
       }
+      this.picNum = node.value
+    },
+    doSomething() {
+      this.$refs.chart.inst.getWidth();
+    },
+    parseSource(data, selected) {
+      let date_set = [];
+      let date_str = [];
+      const date_dict = {};
+      const result_data = {};
+      data.forEach(item => {
+        const m_time = item['m_time'];
+        const time_obj = moment(m_time);
+        date_set.push(time_obj);
+        if(date_dict[item['point_name']]){
+          date_dict[item['point_name']][time_obj.calendar()] = item['value'];
+        }else{
+          date_dict[item['point_name']] = {};
+          date_dict[item['point_name']][time_obj.calendar()] = item['value'];
+        }
+      });
+      date_set = [...new Set(date_set)];
+      date_set.sort((a, b) => a - b);
+      date_set.forEach(item => {
+        date_str.push(item.calendar());
+      });
+      // console.log(date_str);
+      selected.forEach(p => {
+        result_data[p] = [p];    
+      });
+      date_str.forEach(d => {
+        selected.forEach(p => {
+          if(date_dict[p][d]){
+            result_data[p].push(date_dict[p][d]);
+          }else{
+            result_data[p].push(null);
+          }
+        });
+      });
+      date_str = ['date'].concat(date_str);
+      let source = [date_str];
+      selected.forEach(item => {
+        source.push(result_data[item]);    
+      });
+      source = _.zip(...source);
+      // console.log(source);
+      return source;
+    },
+    parseValues(data, selected) {
+      // console.log(data);
+      // console.log(selected);
+      //parse series
+      const series = []
+      const serie_temp = {
+        name: null,
+        type: 'line',
+        encode: {
+            // 将 "amount" 列映射到 X 轴。
+            x: 'date',
+            // 将 "product" 列映射到 Y 轴。
+            y: null
+        },
+        connectNulls:true
+      };
+      selected.forEach(item => {
+        const serie_copy = _.cloneDeep(serie_temp);
+        serie_copy['name'] = item;
+        serie_copy['encode']['y'] = item;
+        series.push(serie_copy);
+      });
+      //parse dataset.source
+      const source = this.parseSource(data, selected);
+
+      return {
+        'legend': selected,
+        'series': series,
+        'source': source
+      }
+    },
+    handleDraw() {
+      let url = 'http://localhost:8000/mvalues/?m_points=';
+      url += _.join(this.selected_points, ',')
+      axios
+        .get(url).then(response => {
+          const result = this.parseValues(response.data, this.selected_points);
+          const new_map = _.cloneDeep(baseMap);
+          new_map['dataset']['source'] = result['source'];
+          new_map['legend']['data'] = result['legend'];
+          new_map['series'] = result['series'];
+          console.log(new_map);
+          this.option = new_map;
+        });
     }
-  };
+  }
+};
 </script>
 <style>
 
 .chart-block {
+  margin-top: 20px;
   height: 700px;
   background-color: white
 }
