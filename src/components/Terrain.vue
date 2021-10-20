7878aa720450d7c9fd1bde5c323848eb1f73c5a1
zfj <zfj>
2021-10-20 18:31:40 +0800

    add accesstoken

--- src/components/Terrain.vue
+++ src/components/Terrain.vue
@@ -55,9 +55,10 @@ export default {
       // viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY)
 
       //  :camera="{position: [101.01,30.155,3000], heading: 20, pitch: -9, roll: 0}"
-      Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0OWY4NGI5NC05Y2E4LTQ0MzgtOGQyMC1lYzYzOTdiZjRlZjciLCJpZCI6Njg4MzQsInNjb3BlcyI6WyJsZ24iLCJwciIsImFjdyIsImFzbCIsImFzciIsImFzdyIsImFzcyIsImdjIiwibHIiLCJsdyIsInRyIiwidHciLCJ1c2ciXSwiaWF0IjoxNjM0MjAzNzg5LCJleHAiOjE2MzQ4MDg1ODl9.E2MbJdLA2b2OehSVd8pHt2hk7rAJwz-7e090eU38bGQ"
+      // Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0OWY4NGI5NC05Y2E4LTQ0MzgtOGQyMC1lYzYzOTdiZjRlZjciLCJpZCI6Njg4MzQsInNjb3BlcyI6WyJsZ24iLCJwciIsImFjdyIsImFzbCIsImFzciIsImFzdyIsImFzcyIsImdjIiwibHIiLCJsdyIsInRyIiwidHciLCJ1c2ciXSwiaWF0IjoxNjM0MjAzNzg5LCJleHAiOjE2MzQ4MDg1ODl9.E2MbJdLA2b2OehSVd8pHt2hk7rAJwz-7e090eU38bGQ"
+      Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2MjVmN2Q4Zi0zMDhlLTQzMTYtYTI4NS0wMmZkNjU1YTJiYzYiLCJpZCI6MjU5LCJpYXQiOjE2MzMxMTA1NTl9.SkjZVHCteXzTifSbSqo2rzG7JLiapcJXK428icJQzMU"
       var tileset = new Cesium.Cesium3DTileset({
-        url: Cesium.IonResource.fromAssetId(637948),
+        url: Cesium.IonResource.fromAssetId(16421),
       });
       viewer.scene.primitives.add(tileset);
       // viewer.scene.camera.setView({
