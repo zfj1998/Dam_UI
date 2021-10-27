823968c6b1ea3ded577b585921c1d977be94274e
zfj <zfj/PC_computer>
2021-10-27 17:14:27 +0800

    show dear dam

--- src/components/Terrain.vue
+++ src/components/Terrain.vue
@@ -12,7 +12,7 @@
       </el-aside>
       <el-main>
         <el-row ref="viewerContainer" class="demo-viewer">
-          <vc-viewer @ready="onViewerReady" :imageryProvider="imageryProvider" :camera="{position: [101.01,30.155,3000], heading: 20, pitch: -9, roll: 0}">
+          <vc-viewer @ready="onViewerReady" :imageryProvider="imageryProvider" :camera="{position: [101.011,30.187,3000], heading: 5, pitch: -20, roll: 0}">
             <vc-provider-terrain-cesium ref="provider"></vc-provider-terrain-cesium>
           </vc-viewer>
         </el-row>
@@ -49,30 +49,15 @@ export default {
       imageryProvider.value = new Cesium.ArcGisMapServerImageryProvider({
         url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
       })
-      // var target = new Cesium.Cartesian3(300770.50872389384, 5634912.131394585, 2978152.2865545116)
-      // var offset = new Cesium.Cartesian3(6344.974098678562, -793.3419798081741, 2499.9508860763162)
-      // viewer.camera.lookAt(target, offset)
-      // viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY)
 
       //  :camera="{position: [101.01,30.155,3000], heading: 20, pitch: -9, roll: 0}"
       // Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0OWY4NGI5NC05Y2E4LTQ0MzgtOGQyMC1lYzYzOTdiZjRlZjciLCJpZCI6Njg4MzQsInNjb3BlcyI6WyJsZ24iLCJwciIsImFjdyIsImFzbCIsImFzciIsImFzdyIsImFzcyIsImdjIiwibHIiLCJsdyIsInRyIiwidHciLCJ1c2ciXSwiaWF0IjoxNjM0MjAzNzg5LCJleHAiOjE2MzQ4MDg1ODl9.E2MbJdLA2b2OehSVd8pHt2hk7rAJwz-7e090eU38bGQ"
-      Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2MjVmN2Q4Zi0zMDhlLTQzMTYtYTI4NS0wMmZkNjU1YTJiYzYiLCJpZCI6MjU5LCJpYXQiOjE2MzMxMTA1NTl9.SkjZVHCteXzTifSbSqo2rzG7JLiapcJXK428icJQzMU"
+      // Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2MjVmN2Q4Zi0zMDhlLTQzMTYtYTI4NS0wMmZkNjU1YTJiYzYiLCJpZCI6MjU5LCJpYXQiOjE2MzMxMTA1NTl9.SkjZVHCteXzTifSbSqo2rzG7JLiapcJXK428icJQzMU"
+      Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MTUwZDQ3NS1mYzY5LTRiNzAtYjk5YS1hZGRiZTYzZTgyZGYiLCJpZCI6Njg4MzQsImlhdCI6MTYzMjg5NTExN30.qPq9W2KiIj5bPvreaPNTcSXJcdk-EB_egnG0zXAtmvM"
       var tileset = new Cesium.Cesium3DTileset({
-        url: Cesium.IonResource.fromAssetId(16421),
+        url: Cesium.IonResource.fromAssetId(653776),
       });
       viewer.scene.primitives.add(tileset);
-      // viewer.scene.camera.setView({
-      //   destination: new Cesium.Cartesian3(
-      //     4401744.644145314,
-      //     225051.41078911052,
-      //     4595420.374784433
-      //   ),
-      //   orientation: new Cesium.HeadingPitchRoll(
-      //   5.646733805039757,
-      //       -0.276607153839886,
-      //       6.281110875400085
-      //   ),
-      // });
     }
     return {
       provider,
